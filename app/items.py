fd78181a52f3d35d5e3f6c68e6cf510fafcff7f7
Historia Montague <historia.montague@outlook.com>
2023-06-05 16:44:08 -0600

    updated lock file with click

--- app/items.py
+++ app/items.py
@@ -16,10 +16,10 @@ class Item():
     
     @id.setter
     def id(self, id):
-        if not hasattr(self, "id"):
+        if not hasattr(self, "id") and isinstance(id, int):
             self._id = id
         else:
-            raise AttributeError
+            raise AttributeError("id is immutable and must be of type int")
         
     @property
     def item_name(self):
@@ -30,7 +30,7 @@ class Item():
         if isinstance(item_name, str):
             self._item_name = item_name
         else:
-            raise AttributeError
+            raise AttributeError("item_name must be of type str")
         
     @property
     def item_description(self):
@@ -41,7 +41,8 @@ class Item():
         if isinstance(item_description, str):
             self._item_description = item_description
         else:
-            raise AttributeError
+            raise AttributeError("Item_description must be of type str")
+    
         
     @property
     def item_type(self):
@@ -52,7 +53,7 @@ class Item():
         if isinstance(item_type, str):
             self._item_type = item_type
         else:
-            raise AttributeError
+            raise AttributeError("Item_type must be of type str")
     
     @classmethod
     def find_item_by_id(cls, id):

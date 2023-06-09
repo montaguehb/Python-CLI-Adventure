6f00991272eea790b3af1b91356a777f4d771d75
rcblake <127946751+rcblake@users.noreply.github.com>
2023-06-08 17:09:11 -0700

    boss and game complete logic

--- app/term.py
+++ app/term.py
@@ -4,17 +4,22 @@ from scripts import *
 
 stdin = click.get_text_stream("stdin")
 
+
 def main():
     character = start()
-    click.echo(f"""{character.username}, your code is under attack! an army of nefarious beings 
+    click.echo(
+        f"""{character.username}, your code is under attack! an army of nefarious beings 
                is attempting to take over the world and they're starting with your ability to 
                have clean version control.  Those enemies have taken all of your Git commands 
-               and scattered them throughout the Git Graveyard, but you can get them back!""")
+               and scattered them throughout the Git Graveyard, but you can get them back!"""
+    )
     game(character)
-    
+
+
 def start():
     click.echo(read("./app/txt/welcome.txt"))
     return char_type()
+
 
 def create_new_char():
     new_char = None
@@ -27,6 +32,7 @@ def create_new_char():
             return new_char
         else:
             click.echo("User already exists")
+
 
 def game(character):
     inv = inventory.Inventory(character)
@@ -44,6 +50,7 @@ def game(character):
     end()
     return False
 
+
 def old_char():
     old_char = None
     while not old_char:
@@ -53,7 +60,8 @@ def old_char():
         if old_char:
             return old_char
         else:
-            click.echo("User does not exist") 
+            click.echo("User does not exist")
+
 
 def char_type():
     user_type = ""
@@ -62,14 +70,16 @@ def char_type():
         user_type = click.prompt("New character", type=str).lower()
         check_exit(user_type)
         click.echo("Before you venture forward, what is your name my brave soul?")
-        if user_type == 'y':
+        if user_type == "y":
             return create_new_char()
-        elif user_type == 'n':
+        elif user_type == "n":
             return old_char()
-               
+
+
 def check_exit(string):
     if string == ".exit":
         sys.exit()
+
 
 def combat(inventory, floor, character):
     if floor.is_enemy_defeated():
@@ -79,29 +89,33 @@ def combat(inventory, floor, character):
         attack = click.prompt("Attack", type=str)
         check_exit(attack)
         show_commands(floor)
-        floor.attack(attack)    
+        floor.attack(attack)
     if character.health <= 0:
-        return game_over()    
-                       
+        return game_over()
+
+
 def move(floor):
     directions = [key for key, value in floor.room.directions.items() if value > 0]
     direction = ""
-    # room = click.prompt("room [yn]", type=str)
-    # if room == "y":
-    #     room_id = click.prompt("room_id", type=int)
-    #     floor.update_room(room_id)  
-    # else:
-    click.echo(f"Which direction do you want to move {directions}")
+    room = click.prompt("room [yn]", type=str)
+    if room == "y":
+        room_id = click.prompt("room_id", type=int)
+        floor.update_room(room_id)
+    else:
+        click.echo(f"Which direction do you want to move {directions}")
     while direction not in directions:
         direction = click.prompt("Direction", type=str).lower()
         check_exit(direction)
         if direction in directions:
             floor.update_room(floor.room.directions[direction])
-            click.echo(f"You move {direction} and find yourself in insert floor directions")
+            click.echo(
+                f"You move {direction} and find yourself in insert floor directions"
+            )
         elif direction == "git":
-            show_commands(floor)     
+            show_commands(floor)
         else:
             click.echo("Please input a valid direction")
+
 
 def game_over():
     click.echo("You've died")
@@ -113,18 +127,21 @@ def game_over():
         elif repeat == "n":
             return False
 
+
 def end():
     click.echo()
+
 
 def show_commands(floor):
     click.echo(floor.inventory.items)
 
+
 def read(file):
     with open(file, "r") as file:
         return file.read()
-                   
+
+
 if __name__ == "__main__":
     playing = True
     while playing:
         playing = main()
-        

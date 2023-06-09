45b2462fbada057ed8092aa1c8b9e59cadeeca45
rcblake <127946751+rcblake@users.noreply.github.com>
2023-06-09 08:09:06 -0700

    62 qa pt 2 (#63)
    * permissions testing
    
    * fixed text file causing key error
    
    * Update README.md
    
    * Update adventure.db
    
    * actually fixed inventory having incorrect character_id's
    
    * sync change
    
    * update to db
    
    * updated to db to remove one way
    
    * update to db
    
    * added a space when splitting
    
    * fixed a typo
    
    * fix issue 55. item room text
    
    * fix first room number flub for room text
    
    * Beat boss shows score and scores, but not done
    
    * fix on floor directions message
    
    ---------
    
    Co-authored-by: Historia Montague <historia.montague@outlook.com>

--- app/term.py
+++ app/term.py
@@ -4,7 +4,17 @@ import random
 from scripts import *
 from rich.console import Console
 from rich.theme import Theme
-custom_theme = Theme({"success": "green", "loot": "yellow", "failure": "red", "neutral":"blue", "character":"bold magenta", "starting": "bold cyan"})
+
+custom_theme = Theme(
+    {
+        "success": "green",
+        "loot": "yellow",
+        "failure": "red",
+        "neutral": "blue",
+        "character": "bold magenta",
+        "starting": "bold cyan",
+    }
+)
 console = Console(theme=custom_theme)
 
 stdin = click.get_text_stream("stdin")
@@ -48,6 +58,7 @@ def game(character):
     end()
     return False
 
+
 def old_char():
     old_char = None
     while not old_char:
@@ -57,7 +68,8 @@ def old_char():
         if old_char:
             return old_char
         else:
-            console.print("User does not exist", style="failure") 
+            console.print("User does not exist", style="failure")
+
 
 def char_type():
     user_type = ""
@@ -65,16 +77,23 @@ def char_type():
         console.print(f"Please input one of these values {['y','n']}", style="neutral")
         user_type = click.prompt("New character", type=str).lower()
         check_exit(user_type)
-        console.print("Before you venture forward, what is your name my brave soul?", style="neutral")
-        if user_type == 'y':
+        console.print(
+            "Before you venture forward, what is your name my brave soul?",
+            style="neutral",
+        )
+        if user_type == "y":
             return create_new_char()
         elif user_type == "n":
             return old_char()
 
+
 def check_exit(string):
     if string == ".exit":
-        console.print("""It appears your logic is no match for the treacherous beasts of the Git Graveyard!
-                    We are not surprised as the Git Graveyard is no place for the faint of heart!""", style="failure")
+        console.print(
+            """It appears your logic is no match for the treacherous beasts of the Git Graveyard!
+                    We are not surprised as the Git Graveyard is no place for the faint of heart!""",
+            style="failure",
+        )
         sys.exit()
 
 
@@ -85,51 +104,67 @@ def combat(inventory, floor, character):
     while character.health > 0 and floor.room.enemy.fight_mechanics:
         attack = click.prompt("Attack", type=str)
         check_exit(attack)
-        show_commands() if attack == "git" else floor.attack(attack)                  
+        show_commands() if attack == "git" else floor.attack(attack)
     if character.health <= 0:
         return game_over(floor.room.enemy.enemy_name, character)
-    return True    
+    return True
+
 
 def move(floor):
     directions = [key for key, value in floor.room.directions.items() if value > 0]
     direction = ""
-    console.print(f"Which direction do you want to move {directions}", style="character")
+    console.print(
+        f"Which direction do you want to move {directions}", style="character"
+    )
     while direction not in directions:
         direction = click.prompt("Direction", type=str).lower()
         check_exit(direction)
         if direction in directions:
             floor.update_room(floor.room.directions[direction])
-            console.print(f"You move {direction} and find yourself in insert floor directions", style="neutral")
+            console.print(
+                f"You move {direction}",
+                style="neutral",
+            )
         elif direction == "git":
-            show_commands(floor)     
+            show_commands(floor)
         else:
             console.print("Please input a valid direction", style="failure")
 
+
 def game_over(enemy_name, character):
     click.echo(read_("./app/txt/game_over.txt").format(enemy_name=enemy_name))
+    repeat = ""
     while repeat not in ("y", "n"):
         repeat = click.prompt("play again?", type=str)
         check_exit(repeat)
         if repeat == "y":
             with open("./app/txt/try_again.txt", "r") as file:
                 text = file.read().splitlines()
-                console.print(text[random.randint(0, len(text) - 1)].format(username=character.username), style="success")
+                console.print(
+                    text[random.randint(0, len(text) - 1)].format(
+                        username=character.username
+                    ),
+                    style="success",
+                )
             return True
         elif repeat == "n":
             return False
 
+
 def end():
     click.echo("end")
 
+
 def show_commands(floor):
     console.print(floor.inventory.items, style="loot")
+
 
 def read_(file):
     with open(file, "r") as file:
         return file.read()
 
+
 if __name__ == "__main__":
     playing = True
     while playing:
         playing = main()
-        
